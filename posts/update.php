e4323e061b61db9bcc72ed49429fe37b721d8ff0
Zanibul Shanto <zanibul.shanto@gmail.com>
2023-07-29 04:18:24 +0600

    fix update & improved.

--- posts/update.php
+++ posts/update.php
@@ -6,14 +6,47 @@
     if(isset($_GET['upd_id'])){
         $id = $_GET['upd_id'];
 
+        //first query
+
         $select = $conn->query("SELECT * FROM posts WHERE id = '$id'");
         $select ->execute();
         $rows = $select->fetch(PDO::FETCH_OBJ);
 
+
+        //second query
+
+        if(isset($_POST['submit'])){
+
+          if(empty($_POST['title']) OR empty($_POST['subtitle']) OR empty($_POST['body'])){
+            echo 'input can not be empty';
+          }else{
+
+            $title = $_POST['title'];
+            $subtitle = $_POST['subtitle'];
+            $body = $_POST['body'];
+
+
+
+            $update = $conn->prepare("UPDATE posts SET title = :title, subtitle = :subtitle, 
+            body = :body WHERE id = '$id' ");
+
+            $insert->execute([
+                ':title' => $title,
+                ':subtitle' => $subtitle,
+                ':body' => $body,
+                ':img' => $img,
+                ':user_id' => $user_id,
+                ':user_name' => $user_name,
+
+            ]);
+
+          }
+        }
     }
+    
 ?>
 
-            <form method="POST" action="update.php?upd_id=">
+            <form method="POST" action="update.php?upd_id=<?php echo $id; ?>">
               <!-- Email input -->
               <div class="form-outline mb-4">
                 <input type="text" name="title" value="<?php echo $rows->title; ?>" id="form2Example1" class="form-control" placeholder="title" />
@@ -25,7 +58,7 @@
             </div>
 
               <div class="form-outline mb-4">
-                <input type="text" name="body" value="<?php echo $rows->body; ?>"  id="form2Example1" class="form-control" placeholder="body" />
+                <textarea type="text" name="body" id="form2Example1" class="form-control" placeholder="body" rows="8"> <?php echo $rows->body; ?>"</textarea>
             </div>
 
               
