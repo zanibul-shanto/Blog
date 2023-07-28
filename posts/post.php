32d00d1c39b5d0e05978f917fe5d4c6d24cd54ea
Zanibul Shanto <zanibul.shanto@gmail.com>
2023-07-29 01:43:36 +0600

    fixing date

--- posts/post.php
+++ posts/post.php
@@ -28,7 +28,7 @@
                                 <span class="meta">
                                     Posted by
                                     <a href="#!"> <?php echo $post->user_name; ?> </a>
-                                    <?php echo date('M', strtotime($post->created_at)) . ', ' . date('d', strtotime($post->created_at)) . ', ' . date('Y', strtotime($post->created_at)); ?>
+                                    <?php echo date('M', strtotime($post->created_at)) . ', ' . date('d', strtotime($post->created_at)) . ' ' . date('Y', strtotime($post->created_at)); ?>
                                 </span>
                             </div>
                         </div>
